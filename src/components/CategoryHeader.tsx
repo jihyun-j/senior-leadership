5d7a4611d4837ad6f8d9d9301ac69a6165076356
jihyun-j <jihyun9142@gmail.com>
2024-07-12 00:24:42 +0900

    [chore] JSON placeholder 데이터 입력

---
+++ src/components/CategoryHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useLocation } from "react-router";
+import { navMenu } from "../util/nav";
+
+const CategoryHeader = () => {
+  return <div></div>;
+};
+
+export default CategoryHeader;
