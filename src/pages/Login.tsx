8fa39da0debc3dc425f07a7b029743369bdd4cfd
jihyun-j <jihyun9142@gmail.com>
2024-07-11 21:32:36 +0900

    [feat] Login 1차 완성 - 로그인 후 홈으로 리다이렉트 필요

---
+++ src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import "firebase/compat/auth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/compat/app";
+import { auth } from "../firebase/config";
+import { useEffect } from "react";
+
+const Login = () => {
+  const uiConfig = {
+    signInSuccessUrl: "/",
+    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  };
+
+  useEffect(() => {
+    const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
+      localStorage.setItem("user", JSON.stringify(user?.uid));
+    });
+    return () => unregisterAuthObserver();
+  }, []);
+
+  return (
+    <div>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+    </div>
+  );
+};
+
+export default Login;
